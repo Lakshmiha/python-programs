8bfb755ef0c77dbe1658033ba3b81788d2f7f196
Lakshmi H <100887312+Lakshmiha@users.noreply.github.com>
2024-11-06 11:57:53 +0200

    Add files via upload

--- November/week 1/student(06.11.24).py
+++ November/week 1/student(06.11.24).py
@@ -1,23 +1,32 @@
-student={"Name":"Arjun","Roll_no":"S101","Reg_no":"R1234","Dept":"Computer Science","Semester":1,"Marks":{"Maths":40,"Data Structures":30,"SE":20,"DFCA":20}}
-total_marks=sum(student["Marks"].values())
-if total_marks>=90:
-    grade="A"
-elif total_marks>=82:
-    grade="B"
-elif total_marks>=75:
-    grade="C"
-elif total_marks>=60:
-    grade="D"
-elif total_marks>=50:
-    grade="P"
-else:
-    student.pop("Roll_no")
+student={}
+student["Name"]=input("Enter the name:")
+student["Roll_no"]=input("Enter rollno:")
+student["Reg_no"]=input("Enter the reg no:")
+student["Dept"]=input("Enter the department:")
+student["Semester"]=input("Enter the semester:")
+print("Student details=",student)
+print()
 
-student["total_marks"]=total_marks
-student["grade"]=grade
-print("Updated Student Dictionary:")
-for key,value in student.items():
-    if key=="marks":
-        print(f"{key}:{value}")
-    else:
-        print(f"{key}:{value}")
+
+student["total_marks"]=int(input("Enter the marks out of 100:"))
+print()
+print("dictionary after adding marks:",student)
+print()
+
+
+
+if student["total_marks"]>=90:
+    student["grade"]="A"
+elif student["total_marks"]>=82:
+    student["grade"]="B"
+elif student["total_marks"]>=75:
+    student["grade"]="C"
+elif student["total_marks"]>=60:
+    student["grade"]="D"
+elif student["total_marks"]>=50:
+    student["grade"]="P"
+else:
+    del student["Roll_no"]
+
+print("dictionary after removing Roll_no:",student)
+print()
